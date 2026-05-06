92f07db1514eae9eee88f810654246a8f90a0e0f
Dante <dantepeletti@gmail.com>
2026-05-06 18:00:04 -0300

    creacion de arrays productos y categorias en js, y nuevas imagenes.

---
+++ js/data.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Chess Burger",
+        descripcion: "Una deliciosa hamburguesa con doble de queso fundido.",
+        precio: 12.99,
+        imagen: "img/cheese_burger.jpg",
+        categoria: "Hamburguesas"
+    },
+    {
+        id: 2,
+        nombre: "Burger Vegana",
+        descripcion: "Una deliciosa hamburguesa con doble de queso fundido.",
+        precio: 14.99,
+        imagen: "img/hamburguesa_vegana.avif",
+        categoria: "Hamburguesas"
+    },
+    {
+        id: 3,
+        nombre: "Pizza Pepperoni",
+        descripcion: "Una pizza clásica con pepperoni y queso fundido.",
+        precio: 18.99,
+        imagen: "img/pizza_pepe.jpeg",
+        categoria: "Pizzas"
+    },
+    {
+        id: 4,
+        nombre: "Coca Cola",
+        descripcion: "Una bebida refrescante y deliciosa.",
+        precio: 2.99,
+        imagen: "img/coca.jpg",
+        categoria: "Bebidas"
+    },
+    {
+        id: 5,
+        nombre: "Helado de Chocolate",
+        descripcion: "Un delicioso helado de chocolate cremoso.",
+        precio: 3.99,
+        imagen: "img/helado_choco.jpg",
+        categoria: "Postres"
+    }
+];
+
+const categorias = ["Hamburguesas", "Pizzas", "Bebidas", "Postres"];
